9f3cb33811da2d9b28554d7f7feaf5875131f754
John Mamish <john.mamish@gmail.com>
2021-07-13 17:58:46 -0500

    read directly from opus_demo output binary file

--- reference-implementation/celt_util.c
+++ reference-implementation/celt_util.c
@@ -1,5 +1,8 @@
 #include "celt_util.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /* TF change table. Positive values mean better frequency resolution (longer
    effective window), whereas negative values mean better time resolution
    (shorter effective window). The second index is computed as:
@@ -18,4 +21,61 @@ int32_t get_tf_change(const frame_context_t* ctx, bool tf_select, bool tf_change
     int tf_select_offset = tf_select ? 2 : 0;
     int tf_changed_offset = tf_changed ? 1 : 0;
     return tf_select_table[ctx->LM][transient_offset + tf_select_offset + tf_changed_offset];
+}
+
+/**
+ * Reads the subsequent frame in fp into a newly allocated opus_raw_frame and returns it.
+ *
+ * returns NULL on failure.
+ */
+static opus_raw_frame_t* opus_raw_frame_create_from_file(FILE* fp)
+{
+    opus_raw_frame_t* f = calloc(1, sizeof(opus_raw_frame_t));
+
+    if (fread(&f->length, sizeof(f->length), 1, fp) != 1) goto _exit_fail_2;
+    if (fread(&f->rng_final, sizeof(f->rng_final), 1, fp) != 1) goto _exit_fail_2;
+
+    f->length = __builtin_bswap32(f->length);
+    f->rng_final = __builtin_bswap32(f->rng_final);
+
+    if ((f->data = malloc(f->length)) == NULL) goto _exit_fail_2;
+
+    if (fread(f->data, f->length, 1, fp) != 1) goto _exit_fail_1;
+
+    return f;
+
+_exit_fail_1:
+    free(f->data);
+
+_exit_fail_2:
+    free(f);
+    return NULL;
+}
+
+opus_raw_frame_t* opus_raw_frame_create_from_nth_in_file(const char* name, int frame_number)
+{
+    // open file
+    FILE* fp;
+    if ((fp = fopen(name, "rb")) == NULL) {
+        return NULL;
+    }
+
+    // read frames until we reach the one we want.
+    int i;
+    for (i = 0; i < frame_number; i++) {
+        opus_raw_frame_t* temp = opus_raw_frame_create_from_file(fp);
+        opus_raw_frame_destroy(temp);
+    }
+
+    // read the frame we want
+    opus_raw_frame_t* result = opus_raw_frame_create_from_file(fp);
+    fclose(fp);
+
+    return result;
+}
+
+void opus_raw_frame_destroy(opus_raw_frame_t* f)
+{
+    if (f) free(f->data);
+    free(f);
 }
--- reference-implementation/celt_util.h
+++ reference-implementation/celt_util.h
@@ -5,6 +5,12 @@
 
 #include "frame_context.h"
 
+typedef struct opus_raw_frame {
+    uint32_t length;
+    uint32_t rng_final;
+    uint8_t* data;
+} opus_raw_frame_t;
+
 /**
  * Performs a table lookup into RFC6716 Tables 60 - 63.
  *
@@ -13,5 +19,28 @@
  * @param[in]     tf_changed  This value is the 0 / 1 at the top of tables 60 - 63.
  */
 int32_t get_tf_change(const frame_context_t* ctx, bool tf_select, bool tf_changed);
+
+/**
+ * Reads the n-th frame from an opus file output by opus_demo -e.
+ *
+ * When opus_demo encodes pcm files, the output opus frames are output in a makeshift file format.
+ * Frames are coded back-to-back where each frame consists of the following (big-endian) fields:
+ *
+ *     uint32_t frame_length;
+ *     uint32_t final_rng_value;
+ *     uint8_t  bytes[frame_length];
+ *
+ * Given the filepath of a binary file output by opus_demo, this function returns a newly-
+ * allocated array containing the bytes of the frame.
+ *
+ * @param[in]     name          Filename to be opened.
+ * @param[in]     frame_number  Frame number (0 indexed) to read.
+ * @param[out]    data          After this function executes, opus_raw_frame will
+ *
+ * @return Returns a new opus_raw_frame on success, returns NULL on failure.
+ */
+opus_raw_frame_t* opus_raw_frame_create_from_nth_in_file(const char* name, int frame_number);
+
+void opus_raw_frame_destroy(opus_raw_frame_t* f);
 
 #endif
