31d7ed837e76b27c2e1d5121933ee62d2e101d0f
John Mamish <john.mamish@gmail.com>
2021-08-16 17:37:44 -0500

    add pcm option to wavegen tool

--- tools/wavegen/wavegen.py
+++ tools/wavegen/wavegen.py
@@ -21,38 +21,45 @@ if __name__ == "__main__":
                     help='Wav file to write to')
     ap.add_argument('--amplitude', dest='amplitude', type=float, action='store', default=32768,
                     help='Amplitude of each individual sine wave')
+    ap.add_argument('--pcm', dest='pcm', action='store_true',
+                    help='Omit wav headers, leaving a raw PCM file')
 
     args = ap.parse_args()
 
     FSAMP = 48000
 
-    with wave.open(args.output_file, "wb") as wav_file:
-        # Configure wav file to be 16-bit mono at 48ksps
-        wav_file.setnchannels(1)
-        wav_file.setsampwidth(2)
-        wav_file.setframerate(FSAMP)
-        wav_file.setnframes(args.num_samps)
 
-        # Use scipy to synthesize sine waves
-        wave = np.zeros(args.num_samps)
-        for freq in args.wave_freqs:
-            for samp_num in range(0, args.num_samps):
-                samp = args.amplitude * math.sin((samp_num * 2 * math.pi * freq) / FSAMP)
-                wave[samp_num] += samp
-
-        # Clip
+    # Use scipy to synthesize sine waves
+    output_wave = np.zeros(args.num_samps)
+    for freq in args.wave_freqs:
         for samp_num in range(0, args.num_samps):
-            if (wave[samp_num] >= 32768): wave[samp_num] =  32767
-            if (wave[samp_num] < -32768): wave[samp_num] = -32768
+            samp = args.amplitude * math.sin((samp_num * 2 * math.pi * freq) / FSAMP)
+            output_wave[samp_num] += samp
 
-        # Convert to byte array
-        wave_ints = wave.astype(np.int16)
-        wave_bytes = wave_ints.tobytes()
+    # Clip
+    for samp_num in range(0, args.num_samps):
+        if (output_wave[samp_num] >= 32768): output_wave[samp_num] =  32767
+        if (output_wave[samp_num] < -32768): output_wave[samp_num] = -32768
 
-        # Display
-        if (args.show_graph):
-            plt.plot(range(args.num_samps), wave_ints)
-            plt.show()
+    # Convert to byte array
+    output_wave_ints = output_wave.astype(np.int16)
+    output_wave_bytes = output_wave_ints.tobytes()
 
-        # Write to file
-        wav_file.writeframesraw(wave_bytes)
+    # Display
+    if (args.show_graph):
+        plt.plot(range(args.num_samps), output_wave_ints)
+        plt.show()
+
+    if (args.pcm):
+        with open(args.output_file, "wb") as pcm_file:
+            pcm_file.write(output_wave_bytes)
+    else:
+        with wave.open(args.output_file, "wb") as wav_file:
+            # Configure wav file to be 16-bit mono at 48ksps
+            wav_file.setnchannels(1)
+            wav_file.setsampwidth(2)
+            wav_file.setframerate(FSAMP)
+            wav_file.setnframes(args.num_samps)
+
+            # Write to file
+            wav_file.writeframesraw(output_wave_bytes)
