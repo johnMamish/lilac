aaf4b9a2118cb08b639685d5a986a9c64303bf39
John Mamish <john.mamish@gmail.com>
2021-10-11 16:48:20 -0500

    add some testing code to pvq test

--- reference-implementation/pvq_test.c
+++ reference-implementation/pvq_test.c
@@ -1,10 +1,23 @@
+#include <math.h>
 #include <stdio.h>
 
 #include "pvq.h"
 
+/**
+ *
+ */
+static int get_pulses(int i)
+{
+    if (i < 8) {
+        return i;
+    } else {
+        return (8 + (i % 8)) << ((i / 8) - 1);
+    }
+}
+
 int main()
 {
-    pvq_family_t s = {25, 25};
+    pvq_family_t s = {5, 30};
 #if 1
     printf("Calculating size of pvq family S(%i, %i):\n", s.l, s.k);
     printf("%10lu\n", pvq_family_calculate_size(&s));
@@ -26,12 +39,26 @@ int main()
     for (int i = 0; i < t.l; i++) printf("% 5i,", v->x[i]);
     printf("\n\n");
 
-#if 1
+#if 0
     t = (pvq_family_t){25, 25};
     b = 3923;
     printf("Calculating vector for code b = %li in codebook S(%i, %i)\n", b, t.l, t.k);
     v = pvq_vector_create_from_codename(&t, b);
     for (int i = 0; i < t.l; i++) printf("% 5i,", v->x[i]);
+    printf("\n\n");
+#endif
+
+#if 1
+    pvq_family_t r = {3, 2};
+    printf("Listing all vectors in family S(%i, %i)\n", r.l, r.k);
+    printf("------------------------------------------------\n");
+    for (int i = 0; i < pvq_family_calculate_size(&r); i++) {
+        pvq_vector_t* v = pvq_vector_create_from_codename(&r, i);
+        printf("%3i: ", i);
+        for (int i = 0; i < r.l; i++) printf("%3i,", v->x[i]);
+        printf("\n");
+        pvq_vector_destroy(v);
+    }
     printf("\n\n");
 #endif
 
@@ -46,6 +73,33 @@ int main()
         printf("\n");
     }
 #endif
+
+    pvq_family_t u;
+    printf("bit cache"
+           "------------------------------------------------\n");
+
+    //static int lvals[] = {1, 2, 3, 4, 6, 8, 9, 11, 12, 16, 18, 22, 24, 32, 36, 44, 48, 64, 72, 88, 96, 144, 176};
+    static int lvals[] = {1, 2, 3, 4, 6, 9, 11, 8, 12, 18, 22, 16, 24, 36, 44, 32, 48, 72, 88, 64, 96, 144, 176};
+    for (int i = 0; i < (sizeof(lvals) / sizeof(lvals[0])); i++) {
+        u.l = lvals[i];
+        float bits;
+        int idx = 0;
+        //printf("L = %3i:", u.l);
+        while (idx < 41) {
+            u.k = get_pulses(idx);
+            bits = 8 * log2(pvq_family_calculate_size(&u));
+            int bitsi = (int)floor(bits - 1e-6);
+            if (bitsi > 255) {
+                break;
+            } else {
+                //printf("%6.1f,", bits);
+                printf("%5i,", (int)floor(bits - 1e-9));
+            }
+
+            idx++;
+        }
+        printf("\n");
+    }
 
     return 0;
 }
